a6261db0757769ceaa61cf89619f0568532f61c9
Odilbek-prog <odilbeknormuhamedov7@gmail.com>
2025-04-05 19:50:02 +0500

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import News from "./pages/News";
 import Portfolio from "./pages/Portfolio";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
